f6848963d21404ddb60fe3846ee855b94f08e96d
Student <example@example.com>
2018-12-10 15:10:32 -0800

    Add template for conditional rendering in class App.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {isGameRunning: true};
+  }
+
   render() {
+    const isGameRunning = this.state.isGameRunning;
+    let game;
+
+    if (isGameRunning) {
+      game =  <p>Game Goes Here!</p> ;
+    } else {
+      game = <p> </p>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <p>This is a header!</p>
         </header>
+        {game}
       </div>
     );
   }
